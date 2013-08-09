682fde8058062fbcb90471ecf4864c4fb6a626c3
Brad Touesnard <brad@touesnard.com>
2013-08-09 09:58:35 -0300

    Contact form messages showing up all whacky.

--- contact-form.php
+++ contact-form.php
@@ -5,26 +5,26 @@
 				<div class="row half">
 					<div class="6u">
 						<input type="text" class="text" name="sender_name" id="sender_name" placeholder="Name" />
-					</div>
-					<div id="error-sender_name-required" class="addon-contact-form-error 6u" style="display: none;">
-						Please enter your name.
+						<div id="error-sender_name-required" class="addon-contact-form-error" style="display: none;">
+							Please enter your name.
+						</div>
 					</div>
 					<div class="6u">
 						<input type="text" class="text" name="sender_email" id="sender_email" placeholder="Email" />
-					</div>
-					<div id="error-sender_email-required" class="addon-contact-form-error 6u" style="display: none;">
-						Please enter your email.
-					</div>
-					<div id="error-sender_email-invalid" class="addon-contact-form-error 6u" style="display: none;">
-						Please enter a valid email address.
+						<div id="error-sender_email-required" class="addon-contact-form-error" style="display: none;">
+							Please enter your email.
+						</div>
+						<div id="error-sender_email-invalid" class="addon-contact-form-error" style="display: none;">
+							Please enter a valid email address.
+						</div>
 					</div>
 				</div>
 				<div class="row half">
 					<div class="12u">
-						<textarea id="message" placeholder="Message" placeholder="Message"></textarea>
-					</div>
-					<div id="error-message-required" class="addon-contact-form-error 12u" style="display: none;">
-						Please enter a message.
+						<textarea id="message" name="message" placeholder="Message"></textarea>
+						<div id="error-message-required" class="addon-contact-form-error" style="display: none;">
+							Please enter a message.
+						</div>
 					</div>
 				</div>
 				<div class="row">
@@ -35,12 +35,12 @@
 					    </ul>
 					</div>
 				</div>
-				<div class="row">
-				<p id="addon-contact-form-success" style="display: none;">
-				    Message sent. Thank you!
-				</p>
-				</div>
 			</div>
 		</form>
+		<div class="row">
+		<p id="addon-contact-form-success" style="display: none;">
+		    Message sent. Thank you!
+		</p>
+		</div>
 	</section>
 <!-- /Contact Form -->
